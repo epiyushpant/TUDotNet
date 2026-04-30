f1b8366d97b368c7e67f43d2c8e1715a186c35d1
Piyush Pant <piyushpant@gmail.com>
2026-04-30 06:33:27 +0545

    changed folders structure for final notes

--- LearnADODotnet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LearnADODotnet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
